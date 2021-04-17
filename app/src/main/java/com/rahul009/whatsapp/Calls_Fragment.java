dae702a89abade6c1e70f3bf086a825919b599f5
Rahul <patil09091997@gmail.com>
2021-04-18 01:21:16 +0530

    main activity

---
+++ app/src/main/java/com/rahul009/whatsapp/Calls_Fragment.java
@@ -0,0 +1,27 @@
+package com.rahul009.whatsapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Calls_Fragment extends Fragment {
+
+
+    public Calls_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_status_, container, false);
+
+        return  view;}
+}
---
+++ app/src/main/java/com/rahul009/whatsapp/Chats_Fragment.java
@@ -0,0 +1,26 @@
+package com.rahul009.whatsapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Chats_Fragment extends Fragment {
+
+    public Chats_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_status_, container, false);
+
+        return  view;
+    }
+}
